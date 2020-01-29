b1e598a6fbcf6757939adc91a64ea1799e66d075
UtkJaiswal <51295665+UtkJaiswal@users.noreply.github.com>
2020-01-29 18:34:24 +0530

    Prithvi web

---
+++ myscripts.js
@@ -0,0 +1,131 @@
+
+
+
+
+
+  function swapContent1(id2) {
+            const main = document.getElementById('list');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+
+
+  function swapContent2(id2) {
+            const main = document.getElementById('list2');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+
+function swapContent3(id2) {
+            const main = document.getElementById('list3');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent4(id2) {
+            const main = document.getElementById('list4');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+
+function swapContent5(id2) {
+            const main = document.getElementById('list5');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent6(id2) {
+            const main = document.getElementById('list6');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent7(id2) {
+            const main = document.getElementById('list7');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent8(id2) {
+            const main = document.getElementById('list8');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent9(id2) {
+            const main = document.getElementById('list9');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent10(id2) {
+            const main = document.getElementById('list10');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent11(id2) {
+            const main = document.getElementById('list11');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+function swapContent12(id2) {
+            const main = document.getElementById('list12');
+            const div = document.getElementById(id2);
+            const clone = div.cloneNode(true);
+
+            while (main.firstChild) main.firstChild.remove();
+
+            main.appendChild(clone);
+        }
+
+
+        
